a935c506ffc606c9be6cbe7c577242fdc82ef0f1
yanggen <yanggen.@qq.com>
2018-11-02 20:51:55 +0800

    Update src/components/footer.vue

---
+++ src/components/footer.vue
@@ -0,0 +1,135 @@
+<template>
+    <div class="app-footer">
+        <footer class="my_footer">
+            <div class="my_content">
+                <div class="my_baozhang">
+                    <ul>
+                        <li>
+                            <img src="http://127.0.0.1:3000/img/weixiu.png" alt="" class="my_img_size">
+                            <a href="#">预约维修服务</a>
+                        </li>
+                        <li>
+                            <img src="http://127.0.0.1:3000/img/qitian.png" alt="" class="my_img_size mt7">  <a href="#">7天无理由退货</a>
+                            
+                        </li>
+                        <li>
+                            <img src="http://127.0.0.1:3000/img/shiwutian.png" alt="">
+                            <a href="#">15五天免费换货</a>
+                        </li>
+                        <li>
+                            <img src="http://127.0.0.1:3000/img/lipin.png" alt="">
+                            <a href="#">满150元包邮</a>
+                        </li>
+                        <li class="bor_none">
+                            <img src="http://127.0.0.1:3000/img/dingwei.png" alt="">
+                            <a href="#">520余家售后网点</a>
+                        </li>
+                    </ul>
+                </div>
+                
+                <div class="my_helf">
+                    <div>
+                        <p class="mb15">帮助中心</p>
+                        <a href="#">账号管理</a><br>
+                        <a href="#">购物指南</a><br>
+                        <a href="#">订单操作</a>
+                    </div>
+                    <div>
+                        <p class="mb15">服务支持</p>
+                        <a href="#">售后政策</a><br>
+                        <a href="#">自助服务</a><br>
+                        <a href="#">相关下载</a>
+                    </div>
+                    <div>
+                        <p class="mb15">线下门店</p>
+                        <a href="#">小米之家</a><br>
+                        <a href="#">服务网点</a><br>
+                        <a href="#">授权体验店</a>
+                    </div>
+                    <div>
+                        <p class="mb15">关于小米</p>
+                        <a href="#">了解小米</a><br>
+                        <a href="#">加入小米</a><br>
+                        <a href="#">投资者关系</a>
+                    </div>
+                    <div>
+                        <p class="mb15">关注我们</p>
+                        <a href="#">新浪微博</a><br>
+                        <a href="#">官方微信</a><br>
+                        <a href="#">联系我们</a>
+                    </div>
+                    <div>
+                        <p class="mb15">特色服务</p>
+                        <a href="#">F码通道</a><br>
+                        <a href="#">礼品码</a><br>
+                        <a href="#">防伪查询</a>
+                    </div>
+
+                    <div class="my_phone400">
+                        <h3>400-100-5678</h3>
+                        <p>周一至周日 8:00-18:00 <br>（仅收市话费） 
+                        </p>
+                        <a href="#" class="my_kefu">联系客服</a>
+                    </div>
+                </div>
+                <div class="site_info">
+                    <div>
+                        <img src="http://127.0.0.1:3000/img/logo.png" alt="">
+                    </div>
+                    <div class="my_href">
+                        <ul>
+                            <li><a href="#" class="ml20">小米商城</a><span> |</span></li>
+                            <li><a href="#">MIUI</a><span> |</span></li>
+                            <li><a href="#">米聊</a><span> |</span></li>
+                            <li><a href="#">米家 </a><span> |</span></li>
+                            <li><a href="#">多看</a><span> |</span></li>
+                            <li><a href="#">游戏</a><span> |</span></li>
+                            <li><a href="#">路由器</a><span> |</span></li>
+                            <li><a href="#">米粉卡</a><span> |</span></li>
+                            <li><a href="#">政企服务 </a><span> |</span></li>
+                            <li><a href="#">小米天猫店</a><span> |</span></li>
+                            <li><a href="#">隐私政策</a><span> |</span></li>
+                            <li><a href="#">MIUI</a><span> |</span></li>
+                            <li><a href="#">问题反馈</a><span> |</span></li>
+                            <li><a href="#">廉政举报</a><span> |</span></li>
+                            <li><a href="#">Select Region</a><span> |</span></li>
+                            <li><img src="http://127.0.0.1:3000/img/v-logo-1.png" alt="" class=""></li>
+                            <li><img src="img/v-logo-2.png" alt=""></li>
+                            <li><img src="http://127.0.0.1:3000/img/v-logo-3.png" alt=""></li>
+                            <li><img src="http://127.0.0.1:3000/img/xin.png" alt=""></li>
+                        </ul>
+                        <div class="benanhao">
+                            <a href="#" class="pl20 pt0">© mi.com</a>
+                            <a href="#">京ICP证110507号</a>
+                            <a href="#">京ICP备10046444号</a>
+                            <a href="#">京公网安备</a>
+                            <a href="#">11010802020134号</a>
+                            <a href="#">网文[2017]1530-131号 </a>
+                            <a href="#">京营业执照</a>
+                            <a href="#">京网文[2017]</a>
+                            <p class="ml20">违法和不良信息举报电话：185-0130-1238，本网站所列数据，除特殊说明，所有数据均出自我司实验室测试</p>
+                            <div>
+                                <img src="http://127.0.0.1:3000/img/slogan2016.png" alt="">
+                            </div>
+                        </div>
+                        
+                    </div>
+                </div>
+            </div>
+        </footer> 
+    </div>
+</template>
+<script>
+import '../../public/css/footer.css'
+export default {
+    data(){
+        return{}
+    },
+    methods:{}
+}
+</script>
+<style>
+
+</style>
+
+
