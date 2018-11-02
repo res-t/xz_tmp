0ae606bc9ebc767b625d4ef652ea4655d82ab208
yanggen <yanggen.@qq.com>
2018-11-02 20:51:44 +0800

    Update src/main.js

--- src/main.js
+++ src/main.js
@@ -2,7 +2,8 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
+import '../public/css/public.css'
+import '../src/assets/jquery-3.2.1'
 Vue.config.productionTip = false
 
 new Vue({
