6cd8a5292dc72aa125139452efc4502168cc3678
yanggen <yanggen.@qq.com>
2018-11-03 18:39:20 +0800

    Update src/views/XmIndex.vue

--- src/views/XmIndex.vue
+++ src/views/XmIndex.vue
@@ -1,5 +1,9 @@
 <template>
     <div class="app-index">
+        <div class="hot_banner">
+            <a href="#" class="hot_banner_link"></a>
+        </div>
+        <xm-header></xm-header>
         <div class="my_swipe">
             <div id="container_img">
                 <a class="item" href="#"><img src="http://127.0.0.1:3000/img/baner.jpg"/></a>
@@ -2112,22 +2116,291 @@
                     </li>
                 </ul>
             </div>
-        </div> 
+        </div>
+        <xm-footer></xm-footer> 
     </div>
 </template>
 <script>
-import '../../public/css/index.css'
+  import '../../public/css/index.css'
+  import xmHeader from '@/components/header.vue'
+  import xmFooter from '@/components/footer.vue'
 export default {
     data(){
         return{}
     },
-    methods:{},
-    mounted(){}
-}
+    mounted(){
+        this.swipe();
+        this.purchas();
+        this.jump();
+        this.clickImg();
+        this.indexLength();
+        this.leftRight();
+        this.piontLi();
+        this.topSwipe();
+        this.videoPlay();
+        this.timeShop();
+        this.phoneApp();
+        this.Roll();
+    },
+    methods:{
+       /**轮播 */
+       swipe(){
+            $(document).ready(function(){
+            var i = 0 ;
+            var timer;
+           //用jquery方法设置第一张图片显示，其余隐藏
+            $('.item').eq(0).show().siblings('.item').hide();
+
+            //调用showTime()函数（轮播函数）
+            showTime();
+
+            //当鼠标经过下面的数字时，触发两个事件（鼠标悬停和鼠标离开）
+            $('.tab').hover(function(){
+                //获取当前i的值，并显示，同时还要清除定时器
+                i = $(this).index();
+                Show();
+                clearInterval(timer);
+            },function(){
+                //
+                showTime();
+            });
+
+            //鼠标点击左侧的箭头
+            $('.prev').click(function(){
+                clearInterval(timer);
+                if(i == 0){
+                i = 5;//注意此时i的值
+                }
+                i--;
+                Show();
+                showTime();
+            });
+
+            //鼠标点击右侧的箭头
+            $('.next').click(function(){
+                clearInterval(timer);
+                if(i == 4){
+                i = -1;//注意此时i的值
+                }
+                i++;
+                Show();
+                showTime();
+            });
+
+
+            //创建一个showTime函数
+            function showTime(){
+            //定时器
+            timer = setInterval(function(){
+                //调用一个Show()函数
+                Show();
+                i++;
+                //当图片是最后一张的后面时，设置图片为第一张
+                if(i==5){
+                i=0;
+                }
+            },5000);
+            }
+
+
+            //创建一个Show函数
+            function Show(){
+            //在这里可以用其他jquery的动画
+            $('.item').eq(i).fadeIn(300).siblings('.item').fadeOut(300);
+
+            //给.tab创建一个新的Class为其添加一个新的样式，并且要在css代码中设置该样式
+            $('.tab').eq(i).addClass('active').siblings('.tab').removeClass('active');
+
+            }
+   
+        });
+
+
+       },
+        /**显示抢购*/
+       purchas(){
+           $(".my_remai_title>a").click(function(){
+                var li = $(this)
+                $(li.attr('data-target')).removeClass("my_display").siblings('.my_Special_procduct').addClass("my_display")
+           })
+       },
+       /**跳转页面 */
+       jump(){
+            $(".appliances_title>div>ul>li").mouseenter(function(){
+                var li = $(this)
+                if( $(li.attr('data-target')).is('.my_display')){
+                $(li.attr('data-target')).removeClass("my_display").siblings(".my_appliances_product").addClass("my_display")
+                li.addClass('my_mouse').siblings('').removeClass("my_mouse")
+                }      
+               li.children().addClass("my_mouse_a").parent().siblings().children().removeClass("my_mouse_a")
+           });
+       },
+       clickImg(){
+           $('.tubook').mouseenter(function(){
+                var b =$(this);
+                b.children(".my_left").removeClass("my_display");
+                b.children('.my_right').removeClass("my_display")
+                }).mouseleave(function(){
+                var b =$(this);
+                b.children('.my_left').addClass("my_display");
+                b.children('.my_right').addClass("my_display")
+            })
+       },
+       indexLength(){
+           for(var i =0;i<$(".ul_length").next().next().next().children('div').length/2-3;i++){
+                var li = $("<li class=''></li>")
+                 // var a=i
+                 li.appendTo(".ul_length");
+                }
+       },
+       /**左右箭头*/
+       leftRight(){
+           var mg=0;var w =240;
+            $(".my_right").click(function(e){
+                e.preventDefault();
+                var rg =$(this);
+                mg++;
+                if(mg<rg.next().children('div').length){
+                    rg.next().css("left",-w*mg)
+                    //console.log(mg);
+                }else{
+                    mg=-1
+                    rg.removeAttr("disabled");
+                }
+                rg.prev().prev().children(`:nth-child(${mg+1})`).addClass('li_bg').siblings().removeClass('li_bg')
+            })
+            $(".my_left").click(function(){
+                mg--
+            var rg =$(this);
+            if(mg>=0){
+                rg.next().next().css("left",-w*mg)
+            }else{
+                mg=0
+            }
+              rg.prev().children(`:nth-child(${mg+1})`).addClass('li_bg').siblings().removeClass('li_bg')
+            })
+    
+    
+       },
+       /**下 圆点*/
+       piontLi(){
+           //第一个li
+            $('.ul_length>li:first-child').addClass('li_bg')
+            //
+            $(".ul_length>li:first-child()").click(function(){
+                var li=$(this)
+                li.addClass('li_bg').siblings().removeClass('li_bg')
+            li.parent().next().next().next().css("left","0px");
+                
+            }).next().click(function(){
+            var li=$(this)
+            li.addClass('li_bg').siblings().removeClass('li_bg')
+            li.parent().next().next().next().css("left","-240px")
+            }).next().click(function(){
+            var li=$(this)
+            li.addClass('li_bg').siblings().removeClass('li_bg')
+            li.parent().next().next().next().css("left","-480px")
+            
+            }) 
+
+       },
+       /**顶部swipe 轮播*/
+       topSwipe(){
+           $(".my_swipe_href>ul>li").mouseenter(function(){
+            var li = $(this)
+            if( $(li.attr('data-target')).is('.my_display')){
+                $(li.attr('data-target')).removeClass("my_display").siblings().addClass("my_display")
+                li.addClass('my_li_bg').siblings('').removeClass("my_li_bg")
+            }
+            })
+            $('.my_swipe_href_xq').prev().parent().mouseleave(function(e){
+                e.preventDefault();
+                $(".my_swipe_href_xq").children("div").addClass("my_display");
+                $(".my_swipe_href>ul>li").removeClass('my_li_bg')
+            })
+       },
+       /**视频播放 */
+       videoPlay(){
+            $(".video_detail>ul>li>a").click(function(){
+                var li = $(this)
+                if( $(li.attr('data-target')).is('.my_display')){
+                    $(li.attr('data-target')).removeClass("my_display").parent().removeClass("my_display")
+                }
+
+            })
+            var v3 = document.getElementById('v3');
+            var  myVideo =  document.querySelector('.my_video');
+            var  videoPlay = document.querySelector('.video_play');
+            for(var i =0;i<document.querySelectorAll("a[class='my_close']").length;i++){
+                var close = document.querySelectorAll("a[class='my_close']");
+               close[i].onclick = function(){
+                v3.pause();
+                videoPlay.classList.add('my_display');
+                myVideo.classList.add('my_display');
+                window.location.reload();
+               }
+           } 
+       },
+       /**限时购定时器*/
+       timeShop(){
+           $(()=>{setInterval(function () {
+                var date = new Date;
+                // console.log(date.getHours())
+                var nowHour = parseInt(date.getHours())
+                // console.log(nowHour)
+                var targetTime = nowHour +1
+                // console.log(targetTime)
+                $(".time-title").html(`${targetTime}:00场`)
+                var nowMinutes = date.getMinutes()
+                var minutes = 60-nowMinutes-1
+                // console.log(60-nowMinutes-1)
+                if(minutes<10){
+                $(".minutes").html(`0${minutes}`)
+                }else{
+                $(".minutes").html(`${minutes}`)
+                }
+                var nowSeconds = date.getSeconds()
+                var seconds = 60-nowSeconds-1
+                if(seconds<10){
+                $(".seconds").html(`0${seconds}`)
+                }else{
+                $(".seconds").html(`${seconds}`)
+                }
+             },1000);
+           })
+       },
+       /**APP--and悬停效果 */
+       phoneApp(){
+           $('.bar-sort').hover(function(){
+             var li =$(this);
+             li.children().children().children().toggleClass("my_display")
+           })
+       },
+       /**返回顶部显示隐藏 滚动条事件*/
+       Roll(){
+           window.onscroll = function(){
+                var docScroll = document.documentElement.scrollTop;
+                 console.log(docScroll,window.innerHeight);
+                if(docScroll>window.innerHeight*2){
+                    console.log(12323);
+                    toptop.classList.remove("my_display")
+                }else{
+                toptop.className="my_display bar-sort"
+                }
+   
+            }
+       }
+    },
+    components:{
+       xmHeader,
+       xmFooter
+     } 
+   
+   
+}  
 </script>
 <style>
 
 </style>
-
 
 
