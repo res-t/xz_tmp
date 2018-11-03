dfd35c4e05cd5c37defc85ec311ac746e728be41
yanggen <yanggen.@qq.com>
2018-11-03 18:39:59 +0800

    Update src/App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,16 @@
 <template>
   <div id="app">
-     <xm-header></xm-header>
       <router-view></router-view>
-      <xm-footer></xm-footer>
   </div>
 </template>
  <script>
- import xmHeader from '@/components/header.vue'
- import xmFooter from '@/components/footer.vue'
+
  export default {
      data(){
        return{}
      },
      methods:{},
-     components:{
-       xmHeader,
-       xmFooter
-     }
+    
  }
  </script>
 
