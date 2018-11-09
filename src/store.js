1866242b908c7fdb2b5c904b2e28561c4b64f55a
yanggen <yanggen.@qq.com>
2018-11-09 20:47:34 +0800

    Update src/store.js

--- src/store.js
+++ src/store.js
@@ -3,14 +3,33 @@ import Vuex from 'vuex'
 
 Vue.use(Vuex)
 
+Vue.use(Vuex)
+const key='user'
 export default new Vuex.Store({
-  state: {
-
+  state(){
+    return{
+      user:null,
+    }   
   },
   mutations: {
-
+    setUser(state,value){
+      state.user=value
+      localStorage.setItem(key,JSON.stringify(value))
+    },
+    removeUser(state){
+      state.user=null
+      localStorage.removeItem(key)
+    }
   },
   actions: {
+    
+  },
+  getters:{
+    getStorage:function(state){
+      if(!state.user){
+        state.user=JSON.parse(localStorage.getItem(key))
+      }
+    },
 
   }
 })
