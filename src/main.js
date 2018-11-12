2fedc58f0ea5790473a915ade690bbfec7e4aa39
yanggen <yanggen.@qq.com>
2018-11-12 21:06:48 +0800

    Update src/main.js

--- src/main.js
+++ src/main.js
@@ -30,6 +30,14 @@ const decodeString = (name) => {
 Vue.prototype.encryptString = encryptString
 Vue.prototype.decodeString = decodeString
 /**end */
+
+router.beforeEach((to, from, next) => {
+    /* 路由发生变化修改页面title */
+    if (to.meta.title) {
+        document.title = to.meta.title
+    }
+    next()
+  })
 new Vue({
   router,
   store,
