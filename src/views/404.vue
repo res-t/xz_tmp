02cecb04acaa4eb6277672b33d2b96c16bd1b2f2
yanggen <yanggen.@qq.com>
2018-11-17 18:44:16 +0800

    Update 404.vue

--- src/views/404.vue
+++ src/views/404.vue
@@ -1,5 +1,9 @@
 <template>
-     <div class="app-404"></div>
+     <div class="app-404">
+         <div class="app-404-btn">
+              <router-link to="/index">返回首页</router-link>
+         </div>
+     </div>
 </template>
 <script>
 export default {
@@ -7,7 +11,31 @@ export default {
 }
 </script>
 <style>
-
+  .app-404{
+       background: url('http://127.0.0.1:3000/detail/404.png') 50% 100% no-repeat;
+       width: 100%;
+      height: 700px;
+      position: relative;
+  }
+  .app-404-btn{
+      width: 100px;
+      height: 100px;
+      position: absolute;
+      bottom: 130px;
+      left: 50%;
+      margin-left: -215px;
+      text-align: center;
+  }
+  .app-404-btn>a{
+       display: block;
+       width: 100px;
+       height: 40px;
+       color: #fff;
+       line-height: 3;
+       border-radius: 10px;
+       background: #C04C55;
+       
+  }
 </style>
 
 
